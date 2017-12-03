2b6e33ae3e1486de014eae5097b5b84a9862a674
Andy Kobyljanec <andy.kobyljanec@mtdproducts.com>
2017-12-03 10:14:00 -0500

    added day 2 part 1

---
+++ 2017_2_1.py
@@ -0,0 +1,14 @@
+file = open("day2_in.txt", 'r')
+checksum = 0
+
+with open('day2_in.txt') as file:
+    for line in file:
+        data = line.split('\t')
+        data = [int(i) for i in data]
+        data_min = min(data)
+        data_max = max(data)
+        print("data: {}, min: {}, max: {}".format(data, data_min, data_max))
+        row_checksum = data_max - data_min
+        checksum += row_checksum
+
+print(checksum)
