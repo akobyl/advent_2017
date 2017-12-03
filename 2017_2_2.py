771f079914fc6e6bd55a383e1d5dfa728ce718c6
Andy Kobyljanec <andy.kobyljanec@mtdproducts.com>
2017-12-03 10:32:15 -0500

    added day 2 part 2

---
+++ 2017_2_2.py
@@ -0,0 +1,20 @@
+file = open("day2_in.txt", 'r')
+checksum = 0
+
+with open('day2_in.txt') as file:
+    for line in file:
+        data = line.split('\t')
+        data = [int(i) for i in data]
+
+        dividers_found = False
+        while not dividers_found:
+            for loc1, d1 in enumerate(data):
+                for loc2, d2 in enumerate(data):
+                    result = d1 / d2
+                    if result - int(result) == 0 and loc1 != loc2:
+                        print('Dividers found: {}/{}'.format(d1,d2))
+                        checksum += result
+                        dividers_found = True
+
+
+print(checksum)
