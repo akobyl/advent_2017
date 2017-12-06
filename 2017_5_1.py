dd781b81af0c138547d16f5c1da8f214d2245182
Andy Kobyljanec <andy.kobyljanec@mtdproducts.com>
2017-12-05 22:45:56 -0500

    completed day 5!

---
+++ 2017_5_1.py
@@ -0,0 +1,24 @@
+jumps = 0
+list = []
+
+with open('day5_in.txt') as file:
+    for line in file:
+        list.append(int(line))
+
+list_len = len(list)
+position = 0
+escaped = False
+
+while not escaped:
+    old_position = position
+    position += list[position]
+    list[old_position] += 1
+    jumps += 1
+    print('jumped from {} to {}'.format(old_position, position))
+
+    if position >= list_len:
+        escaped = True
+        print('escaped!!')
+
+print('-----')
+print(jumps)
---
+++ 2017_5_2.py
@@ -0,0 +1,29 @@
+jumps = 0
+list = []
+
+with open('day5_in.txt') as file:
+    for line in file:
+        list.append(int(line))
+
+list_len = len(list)
+position = 0
+escaped = False
+
+while not escaped:
+    old_position = position
+    position += list[position]
+    if list[old_position] >= 3:
+        list[old_position] -= 1
+    else:
+        list[old_position] += 1
+
+    jumps += 1
+    # this takes too long to print!
+    # print('jumped from {} to {}'.format(old_position, position))
+
+    if position >= list_len:
+        escaped = True
+        print('escaped!!')
+
+print('-----')
+print(jumps)
