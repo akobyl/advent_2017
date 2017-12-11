ed83b8524a36b8a00324a0ebcb96592aad5c4e4f
Andy Kobyljanec <kobyl125@gmail.com>
2017-12-11 16:28:58 -0500

    Added day 11 and day 3 part 2

---
+++ 2017_11.py
@@ -0,0 +1,55 @@
+with open('day11_in.txt') as file:
+    INPUT = file.read().rstrip()
+
+steps = INPUT.split(',')
+
+class HexCube(object):
+    """
+    Hex cube inspired by blog post: https://www.redblobgames.com/grids/hexagons/
+    """
+    x = 0
+    y = 0
+    z = 0
+
+    def move_direction(self, move):
+        if move == 'n':
+            self.y += 1
+            self.z -= 1
+        elif move == 'ne':
+            self.x += 1
+            self.z -= 1
+        elif move == 'se':
+            self.x += 1
+            self.y -= 1
+        elif move == 's':
+            self.y -= 1
+            self.z += 1
+        elif move == 'sw':
+            self.x -= 1
+            self.z += 1
+        elif move == 'nw':
+            self.x -= 1
+            self.y += 1
+        else:
+            raise AssertionError('unknown direction')
+
+    def print_coordinates(self):
+        print('({}, {}, {})'.format(self.x, self.y, self.z))
+
+    def distance_to_origin(self):
+        return (abs(self.x) + abs(self.y) + abs(self.z)) / 2
+
+hc = HexCube()
+
+max_distance = 0
+
+for step in steps:
+    hc.move_direction(step)
+    if hc.distance_to_origin() > max_distance:
+        max_distance = hc.distance_to_origin()
+
+print('-----\npart1')
+print(hc.distance_to_origin())
+
+print('-----\npart2')
+print(max_distance)
--- 2017_3_2.py
+++ 2017_3_2.py
@@ -1,18 +1,71 @@
-INPUT = 12
+INPUT = 289326
 current_sum = 1
-
+answer_found = False
 last_ring = [1]
 current_ring = []
 ring_width = 1
 
-ring_id = 0
+ring_id = 1
+spiral = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
 
-while INPUT > current_sum:
-    ring_id += 1
+
+def convert_xy_to_spiral(x, y, ring_width):
+    center = int(ring_width / 2)
+    return center + x, center + y
+
+
+while not answer_found:
+    # create a new ring
     ring_count = ring_width * 4 + 4
     ring_width += 2
-    current_ring = []
-    current_loc = 0
-    while current_loc < ring_count:
 
-        current_loc += 1
+    new_spiral = [[0] * (2 + ring_width)]
+    for row in spiral:
+        new_spiral.append([0, *row, 0])
+    new_spiral.append([0] * (2 + ring_width))
+    spiral = new_spiral
+
+    # step through ring and add sums
+    max_offset = int((ring_width - 1) / 2)
+    dx = 0
+    dy = 1
+
+    current_x = ring_id
+    current_y = -1 * (max_offset - 1)
+    for i in range(ring_count):
+        x, y = convert_xy_to_spiral(current_x, current_y, ring_width + 2)
+        spiral[y][x] += spiral[y][x + 1]
+        spiral[y][x] += spiral[y][x - 1]
+        spiral[y][x] += spiral[y + 1][x + 1]
+        spiral[y][x] += spiral[y + 1][x - 1]
+        spiral[y][x] += spiral[y - 1][x + 1]
+        spiral[y][x] += spiral[y - 1][x - 1]
+        spiral[y][x] += spiral[y + 1][x]
+        spiral[y][x] += spiral[y - 1][x]
+
+        current_sum = spiral[y][x]
+
+        if spiral[y][x] >= INPUT and not answer_found:
+            print('answer found: {}'.format(spiral[y][x]))
+            answer_found = True
+
+        if abs(current_x) == max_offset and dx != 0:
+            if dx == 1:
+                dy = 1
+            elif dx == -1:
+                dy = -1
+
+            dx = 0
+
+        elif abs(current_y) == max_offset and dy != 0:
+            if dy == 1:
+                dx = -1
+            elif dy == -1:
+                dx = 1
+
+            dy = 0
+
+        current_x += dx
+        current_y += dy
+
+    ring_id += 1
